8ed1320f35c2396a283c01038ab6fd4b11df5d0b
Jackbringas <102544709+Jackbringas@users.noreply.github.com>
2025-03-27 17:47:04 +0000

    added missing tests installed jest change the way function were declared

--- .learn/resets/00-welcome/app.js
+++ .learn/resets/00-welcome/app.js
@@ -1,18 +1,18 @@
 // Declaramos una función con el nombre exacto "fromEuroToDollar"
-const fromEuroToDollar =(valueInEuro) => {
+const fromEuroToDollar = function(valueInEuro) {
     // Convertimos el valor a dólares
     let valueInDollar = valueInEuro * 1.07;
     // Retornamos el valor en dólares
     return valueInDollar;
 }
 
-const fromDollarToYen =(valueInDollar) => {
+const fromDollarToYen = function(valueInDollar) {
     let valueInEuro = valueInDollar / 1.07;
     let valueInYen= valueInEuro * 156.5; 
     return valueInYen
 }
 
-const fromYenToPound = (valueInYen) => {
+const fromYenToPound = function(valueInYen) {
     let valueInEuro = valueInYen / 156.5;
     let valueInPound =  valueInEuro* 0.87;
     return valueInPound
--- test.js
+++ test.js
@@ -10,7 +10,7 @@ test('adds 14 + 9 to equal 23', () => {
     expect(total).toBe(23);
 });
 
-test("One euro should be 1.07 dollars", function() {
+test("One euro should be 1.07 dollars", () => {
     // Importo la funcion desde app.js
     const { fromEuroToDollar } = require('./app.js');
 
@@ -21,5 +21,20 @@ test("One euro should be 1.07 dollars", function() {
     const expected = 3.5 * 1.07;
 
     // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(dollars).toBe(expected); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
+
+test("1.07 dollars should be 156.5 yen", () => {
+    const { fromDollarToYen } = require('./app.js');
+    const yen = fromDollarToYen(1.07);
+    const expected = 1.07/1.07 * 156.5;
+    expect(yen).toBe(expected); 
+})
+
+test("156.5 yen should be 0.87 pounds", () => {
+    const { fromYenToPound } = require('./app.js');
+    const pounds = fromYenToPound(156.5);
+    const expected = 156.5/156.5 * 0.87;
+    expect(pounds).toBe(expected); 
+})
+
